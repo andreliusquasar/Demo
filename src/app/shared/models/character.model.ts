f476a8964d516a617a4c54e0521527cc41d24639
Lius <andrelius.quasar@gmail.com>
2022-03-10 17:46:53 -0300

    change character model

--- src/app/shared/models/character.model.ts
+++ src/app/shared/models/character.model.ts
@@ -1,14 +1,32 @@
 export interface ICharacter {
-    id:	        number;
-    name:	    string;
-    status:	    string;
-    species:	string;
-    type:	    string;
-    gender:	    string;
-    origin:	    object;
-    location:	object;
-    image:	    string;
-    episode:	string[];
-    url:	    string;
-    created:	string;
+    info: IInfo,
+    results: ICharacterSchema[]
 }
+
+export interface IInfo {
+        count: number;
+        pages: number;
+        next: string;
+        prev: any;
+}
+
+export interface ICharacterSchema {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: IAtribute,
+    location: IAtribute,
+    image: string;
+    episode: string[],
+    url: string;
+    created: string;
+}
+
+export interface IAtribute {
+    name: string;
+    url: string;
+}
+  
