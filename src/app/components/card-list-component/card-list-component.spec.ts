f96a553633c609b65eec910191efe3289195721c
Lius <andrelius.quasar@gmail.com>
2022-03-10 22:06:47 -0300

    config card-list

--- src/app/components/card-list-component/card-list-component.spec.ts
+++ src/app/components/card-list-component/card-list-component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CardListComponent } from './card-list-component.component';
+import { CardListComponent } from './card-list-component';
 
 describe('CardListComponent', () => {
   let component: CardListComponent;
