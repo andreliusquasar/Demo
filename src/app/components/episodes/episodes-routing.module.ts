04aed61d364099b7f42b718f9f67ed47b7a84c07
Lius <andrelius.quasar@gmail.com>
2022-03-12 09:28:37 -0300

    add route in episodes routing

--- src/app/components/episodes/episodes-routing.module.ts
+++ src/app/components/episodes/episodes-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EpisodeDetailComponent } from './episode-detail/episode-detail.component';
 import { EpisodesComponent } from './episodes.component';
 
 const routes: Routes = [
     {
         path: '',
         component: EpisodesComponent
+    },
+    {
+        path: 'detail/:id',
+        component: EpisodeDetailComponent
     }
 ];
 
