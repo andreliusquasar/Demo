8cd6e045580ca41fe14aaf68ff536efd68e4ed67
Lius <andrelius.quasar@gmail.com>
2022-03-14 14:10:32 -0300

    config show spinner in component character

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -1,7 +1,9 @@
 import { ServiceRequest } from './../../shared/services/service.service';
 import { Component } from '@angular/core';
-import { pluck } from 'rxjs';
+import { delay, pluck, tap } from 'rxjs';
 import { Router } from '@angular/router';
+
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-characters',
@@ -10,12 +12,15 @@ import { Router } from '@angular/router';
 })
 export class CharactersComponent {
 
-  characters$ = this.service.getCharacters().pipe(pluck('results')); 
+  characters$ = this.service.getCharacters().pipe(delay(400), pluck('results'), tap(() => this.spinner.hide())); 
 
   constructor(
     private service: ServiceRequest,
-    private router: Router
-    ) { }
+    private router: Router,
+    private spinner: NgxSpinnerService
+    ) {
+      this.spinner.show();
+     }
 
   redirectTo(id: number): void {
     this.router.navigate(['characters/detail', id]);
