c06f74147ed1d6b8325becb4e50b987e78faed58
Lius <andrelius.quasar@gmail.com>
2022-03-15 11:45:47 -0300

    config app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,35 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: 'characters',
-        loadChildren: () => import('./components/characters/characters.module').then(c => c.CharactersModule)
-     },
-     {
-        path: 'locations',
-        loadChildren: () => import('./components/locations/locations.module').then(l => l.LocationsModule)
-     },
-     {
-        path: 'episodes',
-        loadChildren: () => import('./components/episodes/episodes.module').then(e => e.EpisodesModule)
-     },
-     {
-        path: 'dashboard',
-        loadChildren: () => import('./components/dashboard/dashboard.module').then(d => d.DashBoardModule)
-     }
-]
+  {
+    path: 'characters',
+    loadChildren: () => import('./components/characters/characters.module').then((c) => c.CharactersModule),
+  },
+  {
+    path: 'locations',
+    loadChildren: () => import('./components/locations/locations.module').then((l) => l.LocationsModule),
+  },
+  {
+    path: 'episodes',
+    loadChildren: () => import('./components/episodes/episodes.module').then((e) => e.EpisodesModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./components/dashboard/dashboard.module').then((d) => d.DashBoardModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'characters',
+  },
+  {
+    path: '**',
+    redirectTo: 'characters',
+  },
+];
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
