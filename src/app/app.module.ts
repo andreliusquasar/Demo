b602d1f76a7473a8e974efba04061cce2b775b74
Lius <andrelius.quasar@gmail.com>
2022-03-16 08:56:48 -0300

    import i18n module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './shared/components-module/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { I18nModule } from './core/i18n.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule, 
-    ComponentsModule
+    ComponentsModule,
+    I18nModule
   ],
   providers: [],
   bootstrap: [AppComponent]
