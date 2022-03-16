358d9a03b9d6ef14ba81cb61302813236dfedce0
Lius <andrelius.quasar@gmail.com>
2022-03-16 13:55:38 -0300

    config chacters translate component

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -1,5 +1,7 @@
+import { NotificationI18nService } from './../../core/notification-language.service';
+import { TranslateService } from '@ngx-translate/core';
 import { ServiceRequest } from './../../shared/services/service.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { delay, pluck, tap } from 'rxjs';
 import { Router } from '@angular/router';
 
@@ -10,20 +12,27 @@ import { NgxSpinnerService } from "ngx-spinner";
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.scss']
 })
-export class CharactersComponent {
+export class CharactersComponent implements OnInit {
 
   characters$ = this.service.getCharacters().pipe(delay(400), pluck('results'), tap(() => this.spinner.hide())); 
 
   constructor(
     private service: ServiceRequest,
     private router: Router,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private translate: TranslateService,
+    private languageService: NotificationI18nService
   ) {
       this.spinner.show();
-     }
+    }
+
+    ngOnInit(): void {
+      this.languageService.getLanguageI18n().subscribe((res: string) => {
+        this.translate.setDefaultLang(res);
+      });
+    }  
 
   redirectTo(id: number): void {
     this.router.navigate(['characters/detail', id]);
   }
-
 }
