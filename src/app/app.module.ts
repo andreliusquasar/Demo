6186b00b1d114df22f2e601fb30668114fa056e6
Lius <andrelius.quasar@gmail.com>
2022-03-16 11:31:56 -0300

    remove import i18n module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './shared/components-module/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { I18nModule } from './core/i18n.module';
 
 
 @NgModule({
@@ -16,8 +15,7 @@ import { I18nModule } from './core/i18n.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule, 
-    ComponentsModule,
-    I18nModule
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
