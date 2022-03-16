7a09a1664d15e49dd721d0f20fc8d3b47f16e599
Lius <andrelius.quasar@gmail.com>
2022-03-16 11:33:45 -0300

    config translate method sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,18 +1,22 @@
+import { NotificationI18nService } from './../../core/notification-language.service';
 import { Component, OnInit } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-
-  constructor(private translate: TranslateService) {
+  
+  constructor(
+    private translate: TranslateService,
+    private languageService: NotificationI18nService
+  ) {
     translate.setDefaultLang('br');
-}
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.languageService.getLanguageI18n().subscribe((res: string) => this.translate.setDefaultLang(res));
+  }
 }
