ccbe33aab15a63706c14f2bf1e2392c2a9a22de2
Lius <andrelius.quasar@gmail.com>
2022-03-17 16:51:48 -0300

    config unit test service request

--- src/app/shared/services/service.service.spec.ts
+++ src/app/shared/services/service.service.spec.ts
@@ -1,16 +1,73 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
-import { ServiceService } from './service.service';
+import { ServiceRequest } from './service.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('ServiceService', () => {
-  let service: ServiceService;
+fdescribe('ServiceRequest', () => {
+  let service: ServiceRequest;
+  let httpMock: HttpTestingController;
+
+  const charactersMock = [
+    {
+      id: 2,
+      name: 'nameMock',
+      status: 'statusMock',
+      species: 'speciesMock',
+      type: 'typeMock',
+      gender: 'genderMock',
+      origin: { name: 'nameMock', url: 'urlMock' },
+      location: { name: 'nameMock', url: 'urlMock' },
+      image: 'imageMock',
+      episode: ['string'],
+      url: 'urlMock',
+      created: 'createdMock',
+    },
+  ];
+
+  const locationsMock = [
+    {
+      id:	        2,
+      name:	    'string',
+      type:	    'string',
+      dimension:	'string',
+      residents:	['string'],
+      url:	    'string',
+      created:	'string',
+    }
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ServiceService);
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [ServiceRequest],
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(ServiceRequest);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call getCharacters method', () => {
+    service.getCharacters().subscribe((res) => {
+      expect(res.length).toBe(1);
+      expect(res).toEqual(charactersMock);
+    });
+    const req = httpMock.expectOne('https://rickandmortyapi.com/api/character');
+    expect(req.request.method).toBe('GET');
+    req.flush(charactersMock);
+  });
+  
+  it('should call getCharactersDetail method', () => {
+    service.getCharactersDetail(1).subscribe((res) => {
+      expect(res).toEqual(charactersMock[0]);
+    });
+    const req = httpMock.expectOne(`https://rickandmortyapi.com/api/character/${1}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(charactersMock[0]);
+  });
+
 });
