825233612bf0a8cf3ec2e18722309c382f9c4897
Lius <andrelius.quasar@gmail.com>
2022-03-18 10:21:06 -0300

    add todo annotation

--- src/app/components/episodes/episode-detail/episode-detail.component.spec.ts
+++ src/app/components/episodes/episode-detail/episode-detail.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EpisodeDetailComponent } from './episode-detail.component';
 
+// TODO: find a way to test graphql
 xdescribe('EpisodeDetailComponent', () => {
   let component: EpisodeDetailComponent;
   let fixture: ComponentFixture<EpisodeDetailComponent>;
