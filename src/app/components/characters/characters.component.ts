85534580eb11a2ad579e952d79194e728804370f
Lius <andrelius.quasar@gmail.com>
2022-03-18 17:05:28 -0300

    code-review characters ts

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -1,10 +1,10 @@
-import { NotificationI18nService } from './../../core/notification-language.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { delay, pluck, tap } from 'rxjs';
+
 import { TranslateService } from '@ngx-translate/core';
 import { ServiceRequest } from '../../shared/services/service-request.service';
-import { Component, OnInit } from '@angular/core';
-import { delay, pluck, tap } from 'rxjs';
-import { Router } from '@angular/router';
-
+import { NotificationI18nService } from './../../core/notification-language.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
@@ -13,6 +13,10 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./characters.component.scss'],
 })
 export class CharactersComponent implements OnInit {
+  /***
+   * It is not correct to put delay in the request.
+   * I added just for example of using the ngx-spinner loading lib added to the project.
+   */
   characters$ = this.service.getCharacters().pipe(
     delay(400),
     pluck('results'),
